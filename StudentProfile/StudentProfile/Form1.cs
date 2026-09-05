101258825ef70ea107c451cd417792831eac388b
Jelyn Alday <jelynalday19@outlook.com>
2026-09-05 21:30:12 +0800

    Add StudentProfile Visual Studio project.

---
+++ StudentProfile/StudentProfile/Form1.cs
@@ -0,0 +1,16 @@
+namespace StudentProfile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("GitHub Beginner Lab","Student Profile");
+
+        }
+    }
+}
